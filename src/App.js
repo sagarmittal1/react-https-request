6acb43c455d2cad58d4fe5a28d3cc0f5bc9f8170
Sagar Mittal <mittalsagar006@gmail.com>
2022-12-18 16:24:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    fetch('https://swapi.dev/api/films')
+      .then((response) => {
+        return response.json();
+      })    
+      .then((data) => {
+        const transformedMovies = data.results.map((movie) => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            releaseDate: movie.release_date,
+            openingText: movie.opening_crawl,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
